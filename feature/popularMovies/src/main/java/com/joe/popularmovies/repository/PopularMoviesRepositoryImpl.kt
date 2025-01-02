9ee109be8198401c60c47a16e8992ba4b364e631
joebarker <joe.joeb29@gmail.com>
2025-01-02 18:48:54 +0700

    Refactor

--- feature/popularMovies/src/main/java/com/joe/popularmovies/repository/PopularMoviesRepositoryImpl.kt
+++ feature/popularMovies/src/main/java/com/joe/popularmovies/repository/PopularMoviesRepositoryImpl.kt
@@ -11,27 +11,36 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import com.joe.data.utils.emitSafeOrFailure
 import com.joe.data.utils.emitSafeOrNone
+import kotlinx.coroutines.flow.FlowCollector
 
 class PopularMoviesRepositoryImpl(
     private val remote: PopularMoviesRemote,
     private val local: PopularMoviesLocal,
 ) : PopularMoviesRepository {
 
-    override suspend fun getPopularMovies(page: Int): Flow<Either<PopularMoviesEntity?, ErrorEntity?>> =
-        flow {
-            val localResult = local.getPopularMovies(page)
-            if(localResult is Either.Success && !localResult.value.movies.isNullOrEmpty()){
-                emitSafeOrNone { localResult.value.toEntity() }
-            }
-            val remoteResult = remote.getPopularMovies(page)
-            remoteResult.fold(
-                onSuccess = {
-                    local.insertPopularMovies(it)
-                    emitSafeOrFailure { it.toEntity() }
-                },
-                onFailure = { emit(Either.Failure(ErrorEntity(it.statusMessage))) }
-            )
-        }
+    override suspend fun getPopularMovies(page: Int): Flow<Either<PopularMoviesEntity?, ErrorEntity?>> = flow {
+        fetchLocal(page)
+        fetchRemote(page)
+    }
 
+    private suspend fun FlowCollector<Either<PopularMoviesEntity?, ErrorEntity?>>.fetchLocal(page: Int) {
+        val localResult = local.getPopularMovies(page)
+        if (localResult is Either.Success && !localResult.value.movies.isNullOrEmpty()) {
+            emitSafeOrNone { localResult.value.toEntity() }
+        }
+    }
+
+    private suspend fun FlowCollector<Either<PopularMoviesEntity?, ErrorEntity?>>.fetchRemote(page: Int) {
+        val remoteResult = remote.getPopularMovies(page)
+        remoteResult.fold(
+            onSuccess = { remoteData ->
+                local.insertPopularMovies(remoteData)
+                emitSafeOrFailure { remoteData.toEntity() }
+            },
+            onFailure = { error ->
+                emit(Either.Failure(ErrorEntity(error.statusMessage)))
+            }
+        )
+    }
 
 }
