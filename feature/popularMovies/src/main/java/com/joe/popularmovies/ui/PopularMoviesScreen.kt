41cbcdf1cd99d5578d3e87e65162719eb2755027
joebarker <joe.joeb29@gmail.com>
2025-01-03 16:33:01 +0700

    Popular movies scroll grid and header

--- feature/popularMovies/src/main/java/com/joe/popularmovies/ui/PopularMoviesScreen.kt
+++ feature/popularMovies/src/main/java/com/joe/popularmovies/ui/PopularMoviesScreen.kt
@@ -1,38 +1,49 @@
 package com.joe.popularmovies.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.SubcomposeAsyncImage
-import com.joe.presentation.R
 import com.joe.popularmovies.presentation.PopularMoviesLoadingMoreState
 import com.joe.popularmovies.presentation.PopularMoviesSuccessState
 import com.joe.popularmovies.presentation.PopularMoviesViewModel
 import com.joe.popularmovies.presentation.model.MovieListItemModel
+import com.joe.presentation.R
 import com.joe.presentation.ui.AnimatedCircularProgressBar
 import com.joe.presentation.ui.ErrorScreen
 import com.joe.presentation.ui.ImageShimmer
@@ -49,12 +60,23 @@ fun PopularMoviesScreen(
 ) {
     LaunchedEffect(Unit) { viewModel.init() }
     val state by viewModel.state.collectAsState()
-
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.fillMaxSize().nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        stringResource(R.string.popularMovies),
+                        style = MaterialTheme.typography.headlineMedium,
+                    )
+                },
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { paddingValues ->
         PullToRefreshBox(
+            modifier = Modifier.padding(paddingValues),
             isRefreshing = state is RefreshingState,
             onRefresh = viewModel::refresh
         ) {
@@ -78,27 +100,51 @@ private fun ScreenState(state: ViewModelState, getMoreMovies: (() -> Unit)?) {
 
 @Composable
 fun PopularMoviesSuccessScreen(state: PopularMoviesSuccessState, getMoreMovies: (() -> Unit)?) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalItemSpacing = 16.dp,
+    ) {
         val movies = state.popularMoviesModel.movies
         items(movies.size) { index ->
             MovieListItem(movies[index])
         }
-//        item {
-//            LaunchedEffect(true) { getMoreMovies?.invoke() }
-//        }
     }
 }
 
 @Composable
 fun MovieListItem(movie: MovieListItemModel) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Box(contentAlignment = Alignment.BottomStart) {
-            PosterImage(movie.posterPath)
-            AnimatedCircularProgressBar(movie.score)
-        }
-        Column{
-            Text(movie.title)
-            Text(movie.releaseDate)
+    Surface(
+        color = MaterialTheme.colorScheme.surface,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+    ) {
+        Column {
+            Box(contentAlignment = Alignment.BottomStart) {
+                PosterImage(movie.posterPath)
+                AnimatedCircularProgressBar(
+                    movie.score,
+                    size = 50f,
+                    numberTextStyle = MaterialTheme.typography.titleSmall,
+                    percentageTextStyle = MaterialTheme.typography.labelSmall,
+                    strokeWidth = 4f,
+                    modifier = Modifier
+                        .padding(start = 16.dp)
+                        .offset(y = 20.dp)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .padding(top = 10.dp)
+            ) {
+                Text(movie.title, style = MaterialTheme.typography.titleMedium)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(movie.releaseDate, style = MaterialTheme.typography.labelMedium)
+            }
         }
     }
 }
@@ -108,8 +154,7 @@ fun MovieListItem(movie: MovieListItemModel) {
 private fun PosterImage(posterImageUrl: String?) {
     Box(
         modifier = Modifier
-            .height(Dimensions.POSTER_IMAGE_HEIGHT.dp)
-            .width(Dimensions.POSTER_IMAGE_WIDTH.dp)
+            .fillMaxWidth()
             .clip(RoundedCornerShape(8))
     ) {
         SubcomposeAsyncImage(
