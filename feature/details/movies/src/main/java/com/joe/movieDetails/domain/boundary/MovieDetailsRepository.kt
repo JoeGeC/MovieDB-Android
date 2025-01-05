4817207872411e235e62adc04efb8a25fb852301
joebarker <joe.joeb29@gmail.com>
2025-01-05 21:29:55 +0700

    Fixed warning

--- feature/details/movies/src/main/java/com/joe/movieDetails/domain/boundary/MovieDetailsRepository.kt
+++ feature/details/movies/src/main/java/com/joe/movieDetails/domain/boundary/MovieDetailsRepository.kt
@@ -3,7 +3,6 @@ package com.joe.movieDetails.domain.boundary
 import com.joe.core.entity.Either
 import com.joe.core.entity.ErrorEntity
 import com.joe.core.entity.MediaDetailsEntity
-import kotlinx.coroutines.flow.Flow
 
 interface MovieDetailsRepository {
     suspend fun getMovieDetails(movieId: Int) : Either<MediaDetailsEntity?, ErrorEntity?>
--- feature/details/movies/src/main/java/com/joe/movieDetails/domain/usecase/MovieDetailsUseCase.kt
+++ feature/details/movies/src/main/java/com/joe/movieDetails/domain/usecase/MovieDetailsUseCase.kt
@@ -2,9 +2,8 @@ package com.joe.movieDetails.domain.usecase
 
 import com.joe.core.entity.Either
 import com.joe.core.entity.ErrorEntity
-import com.joe.movieDetails.domain.boundary.MovieDetailsRepository
 import com.joe.core.entity.MediaDetailsEntity
-import kotlinx.coroutines.flow.Flow
+import com.joe.movieDetails.domain.boundary.MovieDetailsRepository
 
 class MovieDetailsUseCase(
     private val repository: MovieDetailsRepository
