a09521b2a848fb1b3b1d5de38946343f91e22cc8
joebarker <joe.joeb29@gmail.com>
2025-01-08 17:17:44 +0700

    Deleted unnecessary classes

--- feature/details/movies/src/main/java/com/joe/movieDetails/repository/boundary/MovieDetailsRemote.kt
+++
@@ -1,9 +0,0 @@
-package com.joe.movieDetails.repository.boundary
-
-import com.joe.core.entity.Either
-import com.joe.data.response.ErrorResponse
-import com.joe.movieDetails.repository.response.MovieDetailsResponse
-
-interface MovieDetailsRemote {
-    suspend fun getMovieDetails(movieId: Int): Either<MovieDetailsResponse?, ErrorResponse?>
-}
--- feature/details/tv/src/main/java/com/joe/tvDetails/repository/boundary/TvDetailsRemote.kt
+++
@@ -1,9 +0,0 @@
-package com.joe.tvDetails.repository.boundary
-
-import com.joe.core.entity.Either
-import com.joe.data.response.ErrorResponse
-import com.joe.tvDetails.repository.response.TvDetailsResponse
-
-interface TvDetailsRemote {
-    suspend fun getMovieDetails(movieId: Int): Either<TvDetailsResponse?, ErrorResponse?>
-}
