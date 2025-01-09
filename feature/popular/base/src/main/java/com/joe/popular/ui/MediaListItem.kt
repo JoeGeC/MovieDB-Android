6fa9ca07a0d5779e8ef0fba300a3e6a6f55e1d71
joebarker <joe.joeb29@gmail.com>
2025-01-09 21:01:40 +0700

    Removed unused imports

--- feature/popular/base/src/main/java/com/joe/popular/ui/MediaListItem.kt
+++ feature/popular/base/src/main/java/com/joe/popular/ui/MediaListItem.kt
@@ -1,12 +1,10 @@
 package com.joe.popular.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -19,16 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import coil3.compose.SubcomposeAsyncImage
 import com.joe.popular.presentation.model.MediaListItemModel
-import com.joe.presentation.R
 import com.joe.presentation.ui.AnimatedScoreCircle
 import com.joe.presentation.ui.PosterImage
-import com.joe.presentation.ui.ShimmerBox
 
 @Composable
 fun MediaListItem(movie: MediaListItemModel, onClick: (() -> Unit)? = null) {
