9dd48e6b1405be841d1b9a05013fe6252082bcdc
joebarker <joe.joeb29@gmail.com>
2025-01-11 21:02:01 +0700

    Cast converters

--- feature/cast/base/src/main/java/com/joe/cast/presentation/converter/CastPresentationConverterImpl.kt
+++ feature/cast/base/src/main/java/com/joe/cast/presentation/converter/CastPresentationConverterImpl.kt
@@ -1,12 +1,29 @@
 package com.joe.cast.presentation.converter
 
 import com.joe.cast.domain.entity.CastListEntity
+import com.joe.cast.domain.entity.PersonEntity
 import com.joe.cast.presentation.model.CastListModel
+import com.joe.cast.presentation.model.PersonModel
+import com.joe.presentation.formatter.toImageUrl
 
 class CastPresentationConverterImpl : CastPresentationConverter {
 
-    override fun entityToModel(entity: CastListEntity): CastListModel {
-        TODO("Not yet implemented")
-    }
+    override fun entityToModel(entity: CastListEntity): CastListModel =
+        CastListModel(
+            id = entity.id,
+            cast = entity.cast.mapNotNull { it.toModel() },
+            crew = entity.crew.mapNotNull { it.toModel() }
+        )
 
+}
+
+private fun PersonEntity.toModel(): PersonModel? = try {
+    PersonModel(
+        id = this.id,
+        name = this.name,
+        character = this.character,
+        profilePath = this.profilePath?.toImageUrl()
+    )
+} catch (_: Exception){
+    null
 }
--- feature/cast/base/src/main/java/com/joe/cast/repository/converter/CastRepositoryConverter.kt
+++ feature/cast/base/src/main/java/com/joe/cast/repository/converter/CastRepositoryConverter.kt
@@ -1,21 +1,56 @@
 package com.joe.cast.repository.converter
 
+import com.google.gson.reflect.TypeToken
 import com.joe.cast.data.response.CastListResponse
+import com.joe.cast.data.response.PersonResponse
 import com.joe.cast.domain.entity.CastListEntity
+import com.joe.cast.domain.entity.PersonEntity
 import com.joe.cast.local.model.CastListLocalModel
+import com.joe.data.JsonAdapter.localDateGson
 
 class CastRepositoryConverter {
 
     fun localToEntity(localListModel: CastListLocalModel?): CastListEntity? {
-        TODO("Not yet implemented")
+        return localListModel?.let {
+            CastListEntity(
+                id = it.id,
+                cast = convertFromJson(it.cast),
+                crew = convertFromJson(it.crew)
+            )
+        }
     }
 
-    fun remoteToLocal(response: CastListResponse?): CastListLocalModel {
-        TODO("Not yet implemented")
-    }
+    fun convertFromJson(listJson: String): List<PersonEntity> =
+        localDateGson.fromJson<List<PersonEntity>>(
+            listJson,
+            object : TypeToken<List<PersonEntity>>() {}.type
+        ).filterNotNull()
 
-    fun remoteToEntity(response: CastListResponse?): CastListEntity? {
-        TODO("Not yet implemented")
-    }
+    fun remoteToLocal(response: CastListResponse?): CastListLocalModel =
+        CastListLocalModel(
+            id = response?.id ?: throw NullPointerException(),
+            cast = localDateGson.toJson(response.cast),
+            crew = localDateGson.toJson(response.crew)
+        )
 
+    fun remoteToEntity(response: CastListResponse?): CastListEntity? =
+        response?.let {
+            CastListEntity(
+                id = it.id,
+                cast = it.cast.mapNotNull { person -> person.toEntity() },
+                crew = it.crew.mapNotNull { person -> person.toEntity() }
+            )
+        }
+
+}
+
+private fun PersonResponse.toEntity(): PersonEntity? = try {
+    PersonEntity(
+        id = this.id,
+        name = this.name,
+        character = this.character,
+        profilePath = this.profilePath
+    )
+} catch (_: Exception) {
+    null
 }
