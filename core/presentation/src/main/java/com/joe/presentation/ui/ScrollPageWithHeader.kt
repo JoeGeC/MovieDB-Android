bfedf6fc5223062fc952d57eeb94b0c874464ea1
joebarker <joe.joeb29@gmail.com>
2025-01-12 17:29:05 +0700

    Removed unused import directives

--- core/presentation/src/main/java/com/joe/presentation/ui/ScrollPageWithHeader.kt
+++ core/presentation/src/main/java/com/joe/presentation/ui/ScrollPageWithHeader.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -15,7 +14,6 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.unit.dp
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
