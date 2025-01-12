956917f83fede46b51a7e5d2fdecf2f7f30d8c7e
joebarker <joe.joeb29@gmail.com>
2025-01-12 14:04:55 +0700

    CastListLoadingScreen fix

--- feature/cast/base/src/main/java/com/joe/cast/ui/CastLoadingScreen.kt
+++ feature/cast/base/src/main/java/com/joe/cast/ui/CastLoadingScreen.kt
@@ -3,19 +3,16 @@ package com.joe.cast.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -23,88 +20,37 @@ import com.valentinilk.shimmer.shimmer
 
 @Composable
 fun CastListLoadingScreen() {
-    Box(
-        modifier = Modifier
-            .padding(top = 100.dp)
-            .background(MaterialTheme.colorScheme.background)
-    ) {
-        DetailsSurface()
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 42.dp)
-                .shimmer(),
-            verticalAlignment = Alignment.Bottom
-        ) {
-            Box(
-                Modifier
-                    .clip(RoundedCornerShape(8))
-                    .background(MaterialTheme.colorScheme.onSurface)
-            )
-            Spacer(Modifier.weight(1f))
-            Box(
-                Modifier
-                    .background(
-                        color = MaterialTheme.colorScheme.onSurface,
-                        shape = CircleShape
-                    )
-            )
-            Box(Modifier.width(40.dp))
-        }
+    Column(Modifier.shimmer()) {
+        TitleShimmer()
+        ListShimmer()
     }
 }
 
 @Composable
-private fun DetailsSurface() {
-    Surface(
-        modifier = Modifier
-            .padding(top = 140.dp)
-            .fillMaxSize()
-            .clip(
-                RoundedCornerShape(
-                    topStart = 12.dp,
-                    topEnd = 12.dp
-                )
-            ),
-        color = MaterialTheme.colorScheme.surface
+private fun TitleShimmer() {
+    Box(
+        Modifier
+            .padding(top = 16.dp, bottom = 8.dp, start = 26.dp)
+            .height(40.dp)
+            .width(150.dp)
+            .clip(RoundedCornerShape(14.dp))
+            .background(MaterialTheme.colorScheme.onSurface)
+    )
+}
+
+@Composable
+private fun ListShimmer() {
+    LazyRow(
+        Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(horizontal = 26.dp),
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 120.dp, bottom = 16.dp)
-                .padding(horizontal = 28.dp)
-                .shimmer()
-        ) {
+        items(6) {
             Box(
                 Modifier
-                    .padding(top = 12.dp)
-                    .clip(RoundedCornerShape(16))
-                    .fillMaxWidth()
-                    .height(56.dp)
-                    .background(MaterialTheme.colorScheme.onSurface)
-            )
-            Box(
-                Modifier
-                    .padding(top = 6.dp)
-                    .clip(RoundedCornerShape(22))
-                    .width(100.dp)
-                    .height(32.dp)
-                    .background(MaterialTheme.colorScheme.onSurface)
-            )
-            Box(
-                Modifier
-                    .padding(top = 12.dp)
-                    .clip(RoundedCornerShape(20))
-                    .fillMaxWidth()
-                    .height(42.dp)
-                    .background(MaterialTheme.colorScheme.onSurface)
-            )
-            Box(
-                Modifier
-                    .padding(top = 12.dp)
-                    .clip(RoundedCornerShape(4))
-                    .fillMaxWidth()
-                    .height(200.dp)
+                    .padding(horizontal = 8.dp)
+                    .height(380.dp)
+                    .width(Dimensions.PERSON_WIDTH.dp)
+                    .clip(RoundedCornerShape(14.dp))
                     .background(MaterialTheme.colorScheme.onSurface)
             )
         }
